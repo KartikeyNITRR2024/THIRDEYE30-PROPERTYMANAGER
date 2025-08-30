2192d64ce4dc6bbbae5f2d92d1c2c90c474ecfc6
Kartikey2001nitrr <kartikeythawait123@gmail.com>
2025-08-30 18:28:13 +0530

    Updated self client

--- src/main/java/com/thirdeye3/propertymanager/externalcontrollers/SelfClient.java
+++ src/main/java/com/thirdeye3/propertymanager/externalcontrollers/SelfClient.java
@@ -5,8 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.thirdeye3.propertymanager.dtos.Response;
 
-@FeignClient(name = "${spring.application.name}")
+@FeignClient(
+    name = "${spring.application.name}", 
+    url = "${self.url:}"                     
+)
 public interface SelfClient {
+
     @GetMapping("/api/statuschecker/{id}/{code}")
-	Response<String> statusChecker(@PathVariable("id") Integer id, @PathVariable("code") String code);
+    Response<String> statusChecker(
+        @PathVariable("id") Integer id,
+        @PathVariable("code") String code
+    );
 }
+
